142a168bebded9bc17c5cf07ae42603ab0d93899
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-05 14:48:03 +0100

    build unit test (#3)
    * build unit test
    
    * Update setup.py
    
    * Update environment.yml
    
    * Update notebooks.yml
    
    * fix permissions
    
    * Update environment.yml
    
    * Update build_notebooks.sh
    
    * Update notebooks.yml
    
    * Update postBuild
    
    * Update environment.yml

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyiron_experimental',
+    version='0.0.1',
+    description='Repository for experimental plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_experimental',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='hassani@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[
+        'pyiron_base==0.1.45',
+        'temmeta==0.0.6',
+        'pystem==0.0.25',
+        'hyperspy==1.5.2',
+        'scanf==1.5.2',
+    ]
+)
