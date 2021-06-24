b5d780a7b2bb29fcd9cf300ba75b36b2e52c5bea
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-06-24 21:06:10 +0200

    Allow to pass plot_properties

--- pyiron_experimental/tem_analysis.py
+++ pyiron_experimental/tem_analysis.py
@@ -9,22 +9,26 @@ class LineProfile:
         self._signal = emd_signal
         self.fig = self.ax = None
         self._selector = None
+        self._lw = None
 
     def plot_signal(self):
         self.fig, self.ax = plt.subplots()
         self.ax.imshow(self._signal.data)
 
-    def select_line_roi(self):
+    def select_roi(self, lw=5):
+        self._lw = lw
         if self.ax is None:
             self.plot_signal()
         self._selector = ROISelector(self.ax)
-        self._selector.select_line()
+        self._selector.select_line(lw=lw)
 
     @property
     def hs_roi(self):
+        if self._selector is None:
+            raise RuntimeError("You need to select a ROI (select_roi) before this can be converted to hyperspy!")
         scale = self._signal.axes_manager[0].scale
         x = self._selector.x * scale
         y = self._selector.y * scale
         x += self._signal.axes_manager[0].offset
         y += self._signal.axes_manager[1].offset
-        return hs_roi.Line2DROI(x[0], y[0], x[1], y[1], linewidth=5*scale)
+        return hs_roi.Line2DROI(x[0], y[0], x[1], y[1], linewidth=self._lw * scale)
