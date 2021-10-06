c2016042406538e18e098fc7344258fbf0591d86
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-10-06 17:16:31 +0200

    Make new job class available

--- pyiron_experimental/__init__.py
+++ pyiron_experimental/__init__.py
@@ -7,6 +7,7 @@ from pyiron_base import Project, JOB_CLASS_DICT
 JOB_CLASS_DICT["pySTEMTEMMETAJob"] = "pyiron_experimental.pystemjob"
 JOB_CLASS_DICT["TEMMETAJob"] = "pyiron_experimental.temmetajob"
 JOB_CLASS_DICT["MatchSeries"] = "pyiron_experimental.matchseries"
+JOB_CLASS_DICT["HSLineProfiles"] = "pyiron_experimental.tem_analysis"
 
 from ._version import get_versions
 
