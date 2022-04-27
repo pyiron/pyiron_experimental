db7286645eef07842f26b93b46c8db437aea0f23
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-04-27 14:39:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'matplotlib==3.5.1',
         'temmeta==0.0.6',
         'pystem==0.0.26',
-        'hyperspy==1.6.5',
+        'hyperspy==1.7.0',
         'scanf==1.5.2',
     ],
     cmdclass=versioneer.get_cmdclass(),
