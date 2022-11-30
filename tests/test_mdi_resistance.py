6f43aaa52dac0d7c13a873027decbb9d66c8c8ff
Markus Stricker <markus.stricker@rub.de>
2022-11-30 16:30:09 +0100

    Prettified test

--- tests/test_mdi_resistance.py
+++ tests/test_mdi_resistance.py
@@ -1,38 +1,52 @@
-import os
-import numpy as np
-
 from pyiron_base._tests import TestWithCleanProject
-import pyiron_experimental 
 
 
 class TestMDIResistance(TestWithCleanProject):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.data_file = "../notebooks/Ir-Pd-Pt-Rh-Ru_dataset.csv"
-        cls.reference_measurement_indices = [5, 157, 338, 177, 188, 334, 74, 278, 296, 10, 325, 234, 89, 0, 219, 292]
-        
+        cls.reference_measurement_indices = [
+            5,
+            157,
+            338,
+            177,
+            188,
+            334,
+            74,
+            278,
+            296,
+            10,
+            325,
+            234,
+            89,
+            0,
+            219,
+            292,
+        ]
+
     def setUp(self):
-        self.job = self.project.create.job.ResistanceGP('resistance_test')
-        
+        self.job = self.project.create.job.ResistanceGP("resistance_test")
 
     def test_static_workflow(self):
         self.job.input.sample_file = self.data_file
         self.job.input.element_column_ids = [3, -1]
         self.job.run()
 
-        measurement_indices = self.job.output['measurement_indices']
+        measurement_indices = self.job.output["measurement_indices"]
 
-        for value, reference in zip(measurement_indices, self.reference_measurement_indices):
+        for value, reference in zip(
+            measurement_indices, self.reference_measurement_indices
+        ):
             self.assertEqual(value, reference)
-        
 
     def test_load_static_workflow(self):
         self.test_static_workflow()
-        job_load = self.project.load('resistance_test')
+        job_load = self.project.load("resistance_test")
 
-        measurement_indices = job_load.output['measurement_indices']
+        measurement_indices = job_load.output["measurement_indices"]
 
-        for value, reference in zip(measurement_indices, self.reference_measurement_indices):
+        for value, reference in zip(
+            measurement_indices, self.reference_measurement_indices
+        ):
             self.assertEqual(value, reference)
