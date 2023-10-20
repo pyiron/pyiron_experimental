9b0821968293949bb2665bf7b4cbdb95f74471ad
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-10-20 08:18:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     keywords='pyiron',
     packages=find_packages(exclude=["*tests*"]),
     install_requires=[
-        'pyiron_base==0.6.5',
+        'pyiron_base==0.6.6',
         'matplotlib==3.8.0',
         'pystem==0.0.26',
         'hyperspy==1.7.5',
